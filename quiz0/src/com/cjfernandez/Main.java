57c1802c338e1434711fdefbd17e40a588a44947
CJ-Fernandez <cjwyloe.fernandez@benilde.edu.ph>
2021-09-04 21:18:48 +0800

    Answer for Quiz 0

---
+++ quiz0/src/com/cjfernandez/Main.java
@@ -0,0 +1,36 @@
+package com.cjfernandez;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float[] floats = new float[20];
+
+        floats[0] = 69.1f;
+        floats[1] = 69.2f;
+        floats[2] = 69.3f;
+        floats[3] = 69.4f;
+        floats[4] = 69.5f;
+        floats[5] = 69.6f;
+        floats[6] = 69.7f;
+        floats[7] = 69.8f;
+        floats[8] = 69.9f;
+        floats[9] = 44.1f;
+        floats[10] = 44.2f;
+        floats[11] = 44.3f;
+        floats[12] = 44.4f;
+        floats[13] = 44.5f;
+        floats[14] = 44.6f;
+        floats[15] = 44.7f;
+        floats[16] = 44.8f;
+        floats[17] = 44.9f;
+        floats[18] = 9.9f;
+        floats[19] = 9.2f;
+
+        for (int i = 0; i < floats.length; i++)
+        {
+            System.out.println(floats[i]);
+        }
+
+    }
+}
