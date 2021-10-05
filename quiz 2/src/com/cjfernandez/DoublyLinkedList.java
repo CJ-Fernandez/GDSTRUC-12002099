c156170417c8bee74c5a60b4eb0cc396de317743
CJ-Fernandez <cjwyloe.fernandez@benilde.edu.ph>
2021-10-05 13:27:57 +0800

    Answer for Quiz 2

--- quiz 2/src/com/cjfernandez/DoublyLinkedList.java
+++ quiz 2/src/com/cjfernandez/DoublyLinkedList.java
@@ -1,5 +1,40 @@
 package com.cjfernandez;
 
+import org.w3c.dom.Node;
+
 public class DoublyLinkedList {
-    
+    private PlayerNode head, tail;
+
+    public void addNode(Player player)
+    {
+        PlayerNode playerNode = new PlayerNode(player);
+
+        if(head == null){
+            head = tail = playerNode;
+            head.setPrevPlayer(null);
+            tail.setNextPlayer(null);
+        }
+        else
+        {
+            tail.setNextPlayer(playerNode);
+            playerNode.setPrevPlayer(tail);
+            tail = playerNode;
+            tail.setNextPlayer(null);
+        }
+
+    }
+
+    public void printNodes() {
+        PlayerNode current = head;
+        if(head == null) {
+            System.out.println("Doubly linked list is empty");
+            return;
+        }
+        System.out.println("Nodes of doubly linked list: ");
+        while(current != null) {
+            //Print each node and then go to next.
+            System.out.print(current.getPlayer() + " ");
+            current = current.getNextPlayer();
+        }
+    }
 }
--- quiz 2/src/com/cjfernandez/Main.java
+++ quiz 2/src/com/cjfernandez/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Player asuna = new Player(1,"Asuna", 100);
+        Player lethalBacon = new Player(2, "LethalBacon", 205);
+        Player hpDeskjet = new Player(3, "HPDeskjet", 34);
+
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        doublyLinkedList.addNode(asuna);
+        doublyLinkedList.addNode(lethalBacon);
+        doublyLinkedList.addNode(hpDeskjet);
+
+        doublyLinkedList.printNodes();
+
     }
 }
